d5942f6585a3b89ca401bab431e8edd20595aad7
Łukasz Rembecki <lrembecki@hotmail.com>
2019-11-12 22:38:39 +0100

    Readme + culture info

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # DevelopX
-DevelopX chalange
+
+DevelopX chalange project
+
+## Getting Started
+
+### Prerequisities
+
+Application requires docker desktop application
+
+### Installing
+
+Download docker container
+
+```
+docker pull lrembecki/developx-19-11-11
+```
+
+Run docker container
+
+```
+docker run -it --rm -p 80:80 --name developx lrembecki/developx-19-11-11
+```
--- lr.apps.cash-machine/Startup.cs
+++ lr.apps.cash-machine/Startup.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
+using System.Globalization;
 using lr.libs.cash_machine;
 using lr.libs.cash_machine.Services;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -19,6 +22,19 @@ namespace lr.apps.cash_machine
             services.AddTransient<IWithdrawNotesService, WithdrawNotesService>();
             services.AddTransient<ICashMachineService, CashMachineService>();
             services.AddTransient<IAccountBalanceService, MoqAccountBalanceService>();
+
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new List<CultureInfo>
+                {
+                    new CultureInfo("en-US"),
+                    new CultureInfo("en")
+                };
+
+                options.DefaultRequestCulture = new RequestCulture("en-US");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -32,20 +48,14 @@ namespace lr.apps.cash_machine
 
             app.UseStaticFiles();
 
+            app.UseRequestLocalization();
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=CashMachine}/{action=Index}/{id?}"
                 );
-            });
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
             });
         }
     }
